7ef969ad0349656ab89bcea1013a769e4eeed6f1
Gus Caplan <me@gus.host>
2021-02-04 14:57:06 -0600

    testing

--- .ci/pipeline.ts
+++ .ci/pipeline.ts
@@ -1,5 +1,21 @@
-import { Workspace } from "runtime/core.ts";
+import { Workspace, registerPlugin } from "runtime/core.ts";
+import { Button, addButton } from "runtime/button.ts";
+import { GithubCheckRunPlugin } from "pkg/buildy/github@1/plugins.ts";
+
+export async function setup(ws: Workspace) {
+  registerPlugin(new GithubCheckRunPlugin({
+    repositorySlug: 'devsnek/buildy-test',
+  }));
+}
 
 export async function run(ws: Workspace) {
     console.log(`Hello World! (sha = ${ws.sha})`);
+    await addButton(Button.createJob({
+      name: 'demo button',
+      task: '.ci/pipeline.ts:button',
+    }));
+}
+
+export async function button(ws: Workspace) {
+  console.log('The button was pressed!');
 }
