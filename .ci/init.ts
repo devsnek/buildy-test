483dd8673a4bb950ef58fffee73b5868c9cc959d
Gus Caplan <me@gus.host>
2021-02-04 15:23:53 -0600

    more testing

--- .ci/init.ts
+++ .ci/init.ts
@@ -4,5 +4,11 @@ import { GithubCheckRunPlugin } from "pkg/buildy/github@1/plugins.ts";
 export async function setup(ws: Workspace) {
   registerPlugin(new GithubCheckRunPlugin({
     repositorySlug: 'devsnek/buildy-test',
+    name: (ws: Workspace) => {
+      if (ws.job.task === '.ci/pipeline.ts') {
+        return 'Click "Details" to start CI -->';
+      }
+      return ws.job.task;
+    },
   }));
 }
