bc2b8670a81feddbd596928a1c268a75c7acf085
Gus Caplan <me@gus.host>
2021-02-04 15:23:53 -0600

    more testing

--- .ci/pipeline.ts
+++ .ci/pipeline.ts
@@ -2,11 +2,11 @@ import { Workspace } from "runtime/core.ts";
 import { Button, addButton } from "runtime/button.ts";
 
 export async function run(ws: Workspace) {
-    console.log(`Hello World! (sha = ${ws.sha})`);
-    await addButton(Button.createJob({
-      name: 'demo button',
-      task: '.ci/pipeline.ts:button',
-    }));
+  console.log(`Hello World! (sha = ${ws.sha})`);
+  await addButton(Button.createJob({
+    name: 'Start CI (remember to check code!)',
+    task: '.ci/pipeline.ts:button',
+  }));
 }
 
 export async function button(ws: Workspace) {
