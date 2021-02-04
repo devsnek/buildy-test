5b0be66e26c38805f84a2426d4cf2f453cecfc48
Gus Caplan <me@gus.host>
2021-02-04 14:59:49 -0600

    testing

---
+++ .ci/init.ts
@@ -0,0 +1,8 @@
+import { Workspace, registerPlugin } from "runtime/core.ts";
+import { GithubCheckRunPlugin } from "pkg/buildy/github@1/plugins.ts";
+
+export async function setup(ws: Workspace) {
+  registerPlugin(new GithubCheckRunPlugin({
+    repositorySlug: 'devsnek/buildy-test',
+  }));
+}
--- .ci/pipeline.ts
+++ .ci/pipeline.ts
@@ -1,12 +1,5 @@
-import { Workspace, registerPlugin } from "runtime/core.ts";
+import { Workspace } from "runtime/core.ts";
 import { Button, addButton } from "runtime/button.ts";
-import { GithubCheckRunPlugin } from "pkg/buildy/github@1/plugins.ts";
-
-export async function setup(ws: Workspace) {
-  registerPlugin(new GithubCheckRunPlugin({
-    repositorySlug: 'devsnek/buildy-test',
-  }));
-}
 
 export async function run(ws: Workspace) {
     console.log(`Hello World! (sha = ${ws.sha})`);
